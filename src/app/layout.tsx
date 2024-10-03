f453b02dc076ba8cbef2d1c6195e3ef18241ef81
mrfaizee12 <faizee956@gmail.com>
2024-10-04 02:34:07 +0500

    removed footer folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Footer from "./footer/page";
+// import Footer from "./footer/page";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
